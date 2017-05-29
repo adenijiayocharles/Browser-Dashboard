3ec2825c3111ab2041d9ce99be8e65ae72036624
Adeniji Charles ayodipupo <adenijiayocharles@gmail.com>
2017-05-29 16:42:30 +0100

    added date display

--- public/js/time.js
+++ public/js/time.js
@@ -1,18 +1,52 @@
+function addLeadingZeros(i){
+	if(i < 10){
+		i = "0"+ i;
+	}
+	return i;
+}
+
+function getMonthName(month){
+	var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	return monthName[month];
+}
+
+function formatDay(day){
+	if(day == 1){
+		day = day +"st";
+	}else if(day == 2){
+		day = day +"nd";
+	}else if(day == 3){
+		day = day +"rd";
+	}else if(day == 21){
+		day = day +"st";
+	}else if(day == 22){
+		day = day +"nd";
+	}else if(day == 23){
+		day = day +"rd";
+	}else if(day == 31){
+		day = day +"st";
+	}else{
+		day = day +"th";
+	}
+	return day;
+}
+
 (function(){
+	var dateClass = new Date();
+	var year = dateClass.getFullYear();
+	var month = getMonthName(dateClass.getMonth());
+	var day = formatDay(dateClass.getDate());
+	var dateDiv = document.getElementById("date");
+	dateDiv.innerHTML = "<h3>" + day + " of " + month + ", " + year + "<h3>";
+
+
 	setInterval(function(){
-		var hourDiv = document.getElementById("hour");
-		var minutesDiv = document.getElementById("minutes");
-		var secondsDiv = document.getElementById("seconds");
-
-
-		var date = new Date();
-		var hour = date.getHours();
-		var minutes = date.getMinutes();
-		var seconds = date.getSeconds();
-
+		var dateClass = new Date();
+		var timeDiv = document.getElementById("time");
+		var hour = addLeadingZeros(dateClass.getHours());
+		var minutes = addLeadingZeros(dateClass.getMinutes());
+		var seconds = addLeadingZeros(dateClass.getSeconds());
 		//update div with corresponding times
-		hourDiv.innerHTML = "<h3>" + hour + "<h3>";
-		minutesDiv.innerHTML = "<h3>" + minutes + "<h3>";
-		secondsDiv.innerHTML = "<h3>" + seconds + "<h3>";
+		timeDiv.innerHTML = "<h3>" + hour + " : " + minutes + " : " + seconds + "<h3>";
 	}, 1000);
 })();
