fb05cbac59919f183ef96d1419905fbdc59745a5
Adeniji Charles ayodipupo <adenijiayocharles@gmail.com>
2017-05-29 08:56:01 +0100

    added comments to background.js

--- public/js/background.js
+++ public/js/background.js
@@ -7,11 +7,20 @@ var images = [
 ];
 
 document.getElementById("refresh").addEventListener("click", function(){
-	var imagesCount = images.length;
-	var random = Math.floor(Math.random() * imagesCount);
+	//get body element
 	var body = document.getElementById("body");
+
+	//set initial background color before bg image loads
+	body.style.backgroundColor = '#333';
+
+	//get length of image array length
+	var imagesCount = images.length;
+
+	//generate random number with length as max
+	var random = Math.floor(Math.random() * imagesCount);
+	
+	//set background image, repeat and size
 	body.style.backgroundImage  = images[random];
 	body.style.backgroundSize = 'cover';
 	body.style.backgroundRepeat = 'no-repeat';
-	//console.log(images[random]);
 });
